171a19a8ec42b74fa8a0dfcaf4a0e8408990ba67
Md. Rakibul Islam <36030906+RakibulIslam1161062@users.noreply.github.com>
2019-02-01 03:01:11 +0600

    Add files via upload

--- api.js
+++ api.js
@@ -2,18 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Ninja = require('../models/ninja');
 
-router.get('/ninjas',function (req, res) {
-  res.send({type:'GET'});
+router.get('/ninjas',function (req, res, next) {
+  //res.send({type:'GET'});
+//  Ninja.geoNear(
+//    {
+//      type:'Point',coordinates:[parseFloat(req.query.lng),parseFloat(req.query.lat)]
+//    },
+//    {maxDistance: 100000, spherical:true}
+//  ).then(function(ninja){
+  //  res.send(ninja);
+//  });
+Ninja.aggregate([{ $geoNear: { near: {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]}, spherical: true, maxDistance: 100000, distanceField: "dist.calculated" } }]).then(function(results){ res.send(results); });
+
 });
 
- router.post('/ninjas',function (req, res) {
+router.post('/ninjas',function (req, res, next) {
   console.log(req.body);
   Ninja.create(req.body).then(function(ninja){
     res.send(ninja);
-  });
+  }).catch(next);
 
 });
-router.put('/ninjas/:id',function (req, res) {
+router.put('/ninjas/:id',function (req, res, next) {
   res.send({type:'put'});
 });
 
--- index.js
+++ index.js
@@ -12,6 +12,10 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use(routes);
 
+app.use(function(err, req, res, next){
+  //console.log(err);
+  res.status(422).send({error: err.message});
+});
 app.listen(process.env.port || 4000,function () {
   console.log("listening");
 });
