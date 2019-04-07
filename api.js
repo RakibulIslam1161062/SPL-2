51362db5436045cfca079fa2ba34f7720e786ff4
Md. Rakibul Islam <36030906+RakibulIslam1161062@users.noreply.github.com>
2019-04-07 19:15:14 +0600

    Add files via upload

--- api.js
+++ api.js
@@ -7,15 +7,10 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 router.get('/ninjas',  async function  (req, res) {
-
-
     MongoClient.connect(url,async function(err, db) {
   if (err) throw err;
   var dbo = db.db("ninjago");
-  console.log("lolii");
-
-
-
+  console.log("lol");
 
   dbo.collection("ninjas", function(err, collection) {
   collection
@@ -34,21 +29,49 @@ router.get('/ninjas',  async function  (req, res) {
     );
 });
 
-
-
 });
 
 });
 
 
+router.post('/login',  async function  (req, res) {
+    MongoClient.connect(url,async function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("ninjago");
+  console.log("lol2");
+
+
+  dbo.collection('accounts').findOne({ userName: req.body.userName}, function(err, user) {
+        console.log(user.userName);
+        // In case the user not found
+
+        if(err) {
+          console.log('THIS IS ERROR RESPONSE')
+          res.json(err)
+        }
+        if (user.password == req.body.password){
+          console.log('User and password is correct')
+         res.json({userName: user.userName, password: user.password});
+        } else {
+          console.log("Credentials wrong");
+          res.json({userName:"invalid", password: "invalid"});
+        }
+ });
+
+});
+
+});
 
 router.post('/ninjas', function (req, res, next) {
   console.log(req.body);
   Ninja.create(req.body).then(function(ninja){
     res.send(ninja);
   }).catch(next);
+  
 
 });
+
+
 
 router.post('/signup', function (req, res, next) {
   console.log(req.body);
